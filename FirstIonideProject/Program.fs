79a0926cff9ea8cad26d8a4ae6d3238c396118c4
Åsmund Ødegård <asmund.odegard@hafslundeco.no>
2022-11-16 17:19:50 +0100

    test1, interactive

--- FirstIonideProject/Program.fs
+++ FirstIonideProject/Program.fs
@@ -1,2 +1,6 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+
+// Starting at https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-vscode
+
+
 printfn "Hello from F#"

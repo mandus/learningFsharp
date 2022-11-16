67af2583d0317bbc92ae41ea36a33322a9f739fc
Åsmund Ødegård <asmund.odegard@hafslundeco.no>
2022-11-16 21:45:01 +0100

    upd, test1

--- FirstIonideProject/Program.fs
+++ FirstIonideProject/Program.fs
@@ -2,5 +2,24 @@
 
 // Starting at https://learn.microsoft.com/en-us/dotnet/fsharp/get-started/get-started-vscode
 
+// printfn "Hello from F#"
 
-printfn "Hello from F#"
+module PigLatin = 
+    let toPigLatin (word: string) = 
+        let isVowel (c: char)  = 
+            match c with
+            | 'a' | 'e' | 'i' | 'o' | 'u'
+            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+            |_ -> false
+        
+        if isVowel word[0] then
+            word + "yay"
+        else
+            word[1..] + string(word[0]) + "ay"
+
+[<EntryPoint>]
+let main args = 
+    for arg in args do
+        let pigArg = PigLatin.toPigLatin args
+        printfn "%s in Pig Latin is: %s" arg pigArg
+    0 // Need to end with a zero (or some other int) since main should return int.
